2d3a80a005bcd7d7e3a78946c258cf0c25a70fe0
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-02-10 15:46:25 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+Using system
+
+namespace MyDictionary
+{
+      class Program
+      {
+         static void Main(strings[] args)
+        {
+           MyDictionary<int,strings>şehirler1 = New MyDictionary<int,strings>();
+           şehirler1.Add(01,"Adana")
+           şehirler1.Add(37,"İstanbul)
+           şehirler1.Add(03,"Afyon")
+
+           MyDictionary<int,strings>şehirler1 = New MyDictionary<int,strings>();
+           şehirler2.Add(10,"Balıkesir")
+           şehirler2.Add(26,"Eskişehir")
+           şehirler2.Add(42,"Konya")
+
+
+
+      }
+   }
+}
